91f4ac7d96db77d69c3f01051b2bc096b22cf8ba
Jilly-del <93799176+Jilly-del@users.noreply.github.com>
2024-09-09 23:08:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,73 @@
-let content = document.querySelector(".content");
+"use script";
+let form = document.getElementById("form");
 let plan = document.querySelector(".plan");
-let planH = document.querySelector(".plan-h");
-let planBtn = document.querySelector(".plan-btnx");
-let planActive = document.querySelectorAll(".active");
+let planh = document.querySelector(".plan-btnh");
+let planbtn = document.querySelector(".plan-btnx");
+let addon = document.querySelector(".add-on");
+let addbtn = document.querySelector(".add-btn");
+let addh = document.querySelector(".add-h");
+let finish = document.querySelector(".finishing-up");
+let finishBtn = document.querySelector(".finish-btnx");
+let finishH = document.querySelector(".finish-H");
+let thank = document.querySelector(".thanks");
+let one = document.getElementById("one");
 
-document.getElementById("myForm").addEventListener("submit", function (event) {
-  event.preventDefault();
-  document
-    .querySelectorAll(".error")
-    .forEach((error) => error.classList.remove("show"));
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let nameError = document.getElementById("nameError");
+  let emailError = document.getElementById("emailError");
+  let phoneError = document.getElementById("phoneError");
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let isvalid = true;
 
-  const name = document.getElementById("name").value.trim();
-  const email = document.getElementById("email").value.trim();
-  const phone = document.getElementById("phone").value.trim();
+  // Name
+  if (!name) {
+    nameError.classList.add("show");
+    isvalid = false;
+  }
+  if (!email) {
+    emailError.classList.add("show");
+    isvalid = false;
+  }
 
-  let isValid = true;
+  if (!phone) {
+    phoneError.classList.add("show");
+    isvalid = false;
+  }
 
-  //   if (!name) {
-  //     document.getElementById("nameError").classList.add("show");
-  //     isValid = false;
-  //   }
-  //   if (!email) {
-  //     document.getElementById("emailError").classList.add("show");
-  //     isValid = false;
-  //   }
-
-  //   if (!phone) {
-  //     document.getElementById("phoneError").classList.add("show");
-  //     isValid = false;
-  //   }
-
-  if (isValid) {
-    content.style.display = "none";
-    plan.style.display = "block";
+  if (isvalid) {
+    form.classList.add("hidden");
+    plan.classList.remove("hidden");
+    one.style.backgroundColor = "none";
   }
 });
 
-planH.addEventListener("click", function (e) {
-  e.preventDefault();
-  plan.style.display = "none";
-  content.style.display = "block";
+planbtn.addEventListener("click", function () {
+  plan.classList.add("hidden");
+  addon.classList.remove("hidden");
 });
 
-// planActive.forEach((el) => {
-//   el.classList.remove("active");
-//   el.addEventListener("click", function (el) {
-//     el.target.classList.add("active");
-//   });
-// });
+planh.addEventListener("click", function () {
+  form.classList.remove("hidden");
+  plan.classList.add("hidden");
+});
+
+addbtn.addEventListener("click", function () {
+  addon.classList.add("hidden");
+  finish.classList.remove("hidden");
+});
+addh.addEventListener("click", function () {
+  plan.classList.remove("hidden");
+  addon.classList.add("hidden");
+});
+
+finishBtn.addEventListener("click", function () {
+  finish.classList.add("hidden");
+  thank.classList.remove("hidden");
+});
+finishH.addEventListener("click", function () {
+  finish.classList.add("hidden");
+  addon.classList.remove("hidden");
+});
